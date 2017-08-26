94008b476be28618eb0e6cd09c06d0ac3f0f5ec2
xuanmos <xuanmos@vip.qq.com>
2017-08-26 22:23:52 +0800

    禁止emoji表情

--- functions.php
+++ functions.php
@@ -558,6 +558,30 @@ add_filter('comment_form_logged_in_after', 'add_my_tips');
 
 /*
  ****************************************
+ * 禁止emoji表情
+ ****************************************
+ */
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+add_action( 'init', 'disable_emojis' );
+
+/*
+ ****************************************
  * 获取访客VIP样式
  ****************************************
  */
