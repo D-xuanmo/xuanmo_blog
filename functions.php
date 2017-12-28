d7875ddeef40808ee64a8ff71b3837f1ce8df9aa
xuanmos <me@xuanmo.xin>
2017-12-28 10:49:13 +0800

    删除旧版本的点赞代码

--- functions.php
+++ functions.php
@@ -817,32 +817,6 @@ add_action('comment_text', 'comments_embed_img');
 
 /*
  ****************************************
- * 点赞功能
- ****************************************
- */
-add_action('wp_ajax_bigfa_like', 'bigfa_like');
-add_action('wp_ajax_nopriv_bigfa_like', 'bigfa_like');
-function bigfa_like()
-{
-  $id = $_POST["um_id"];
-  $action = $_POST["um_action"];
-  if ($action == 'ding') {
-    $bigfa_raters = get_post_meta($id, 'bigfa_ding', true);
-    $expire = time() + 99999999;
-    $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
-    setcookie('bigfa_ding_' . $id, $id, $expire, '/', $domain, false);
-    if (!$bigfa_raters || !is_numeric($bigfa_raters)) {
-      update_post_meta($id, 'bigfa_ding', 1);
-    } else {
-      update_post_meta($id, 'bigfa_ding', ($bigfa_raters + 1));
-    }
-    echo get_post_meta($id, 'bigfa_ding', true);
-  }
-  die();
-}
-
-/*
- ****************************************
  * 设置文章喜爱度
  ****************************************
  */
